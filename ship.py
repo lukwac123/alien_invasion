259ec46769558261350b688cdc8d8c714b896e37
lukwac123 <lukasz-waclawek@wp.pl>
2024-06-24 22:56:43 +0200

    Dostosowanie szybkości poruszania się statku w poziomie.

--- ship.py
+++ ship.py
@@ -6,7 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Inicjacja statku kosmicznego i jego położenie początkowe."""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings()
 
         # Wczytywanie obrazu statku kosmicznego i pobranie jego prostokąta.
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +14,9 @@ class Ship:
 
         # Każdy nowy statek kosmiczny pojawia się na dole ekranu.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Położenie poziome statku jest przechowywane w postaci liczby zmiennoprzecinkowej.
+        self.x = float(self.rect.x)
 
         # Opcje wskazjące na poruszanie się statku.
         self.moving_right = False
@@ -23,9 +26,12 @@ class Ship:
     def update(self):
         """Uaktualnianie położenia statku na podstawie opcji wskazującej na jego ruch."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Uaktualnienie obiektu rect na podstawie wartości self x.
+        self.rect.x = self.x
 
 
     def blitme(self):
