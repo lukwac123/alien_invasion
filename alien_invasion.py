b90e1fe6207be30e60095b8fb7752e37c5cef29e
lukwac123 <lukasz-waclawek@wp.pl>
2024-06-29 11:11:55 +0200

    Dodanie obcych w rzędzie i dodanie kolejnych rzędów.

--- alien_invasion.py
+++ alien_invasion.py
@@ -94,9 +94,30 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Utworzenie pełnej floty obcych."""
-        # Utworzenie obcego.
+        # Utworzenie obcego i dodawanie kolejnych obcych, którzy zmieszczą się w rzędzie.
+        #Odległość pomiędzy obcymi jest równa szerokości obcego.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+
+            # Ukończenie rzędu, wyzerowanie wartości x oraz inkrementacja wartości y.
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+
+    def _create_alien(self, x_position, y_position):
+        """Utworzenie obcego i umieszczenie go w rzędzie."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
         
 
     def _update_screen(self):
